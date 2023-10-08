7dcd5106bf215302a412eb5a437303a29393c426
serafim <matheus18serafim@gmail.com>
2023-10-08 11:49:11 -0300

    feat:home-page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      white: "#fff",
+
+      gray: {
+        100: "#E1E1E6",
+        200: "#A9A9B2",
+        400: "#7C7C8A",
+        600: "#323238",
+        800: "#202024",
+        900: "#121214",
+      },
+
+      green: {
+        500: "#00B37E",
+        600: "#00875F",
+      },
+    },
+
+    fontFamily: {
+      roboto: "Roboto",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
