e57723bc6e7782ecabf75eae1cdbb29404e7b716
serafim <matheus18serafim@gmail.com>
2023-10-22 15:21:26 -0300

    remove getStaticProps

--- src/pages/schedule/[username]/index.tsx
+++ src/pages/schedule/[username]/index.tsx
@@ -3,31 +3,50 @@ import { GetStaticProps } from "next";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ScheduleForm } from "./scheduleForm";
 import { NextSeo } from "next-seo";
 
 interface IPropsUser {
-  user: {
-    id: string;
-    username: string;
-    name: string | null;
-    bio: string | null;
-    email: string | null;
-    avatarUrl: string | null;
-  };
+  id: string;
+  username: string;
+  name: string | null;
+  bio: string | null;
+  email: string | null;
+  avatarUrl: string | null;
 }
 
-export default function Schedule({ user }: IPropsUser) {
+export default function Schedule() {
   const session = useSession();
   const router = useRouter();
+  const [user, setUser] = useState<IPropsUser>();
 
   const { username } = router.query;
+
+  async function handleGetUser() {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username as string,
+      },
+    });
+
+    if (!user) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+    setUser(user);
+  }
 
   useEffect(() => {
     if (session.status === "unauthenticated") {
       router.push("/");
     }
+
+    handleGetUser();
   }, [session.status]);
 
   return (
@@ -40,15 +59,15 @@ export default function Schedule({ user }: IPropsUser) {
             <Image
               className="rounded-full w-full h-full"
               objectFit="cover"
-              src={user.avatarUrl ?? ""}
+              src={user?.avatarUrl ?? ""}
               width={100}
               height={100}
               alt="foto de perfil"
             />
           </div>
 
-          <h2 className="text-2xl font-bold">{user.name}</h2>
-          <p className="text-gray-400 text-base">{user.bio}</p>
+          <h2 className="text-2xl font-bold">{user?.name}</h2>
+          <p className="text-gray-400 text-base">{user?.bio}</p>
         </div>
 
         <ScheduleForm />
@@ -61,39 +80,5 @@ export const getStaticPaths = async () => {
   return {
     paths: [],
     fallback: "blocking",
-  };
-};
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const username = String(params?.username);
-
-  const user = await prisma.user.findUnique({
-    where: {
-      username,
-    },
-  });
-
-  if (!user) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      user: {
-        id: user.id,
-        username: user.username,
-        name: user.name,
-        bio: user.bio,
-        email: user.email,
-        avatarUrl: user.avatarUrl,
-      },
-    },
-
-    revalidate: 60 * 60 * 24, // 1 hours
   };
 };
